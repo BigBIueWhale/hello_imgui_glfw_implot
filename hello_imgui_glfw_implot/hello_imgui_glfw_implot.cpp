27c66b8b91afcdb16d7ea12753465dc0c8339a40
Ronen Zyroff <rzyroff@gmail.com>
2023-02-02 05:38:43 -0800

    Added implot as submodule

---
+++ hello_imgui_glfw_implot/hello_imgui_glfw_implot.cpp
@@ -0,0 +1,17 @@
+﻿#include "hello_imgui/hello_imgui.h"
+
+static void GuiCode()
+{
+    ImGui::Begin("hello_world");
+    ImGui::Text("Hello, world!");
+    ImGui::End();
+}
+
+int main(const int argc, const char *const *const argv)
+{
+    HelloImGui::Run(
+        [] { GuiCode(); },
+        "Hello!"                             // Window title
+    );
+    return 0;
+}
